152de319b5db7014c3e53ab0bbc2746559eeb8b1
Juan Ignacio Arzoumanian <jarzoumanian@ar0c02fl21eml85.ml.com>
2023-08-30 12:38:12 -0300

    first commit

---
+++ ex1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//DECLARACIONES MUY SIMILAR A C#
+
+func main() {
+	var prueba string = "se viene la 7ma boooca"
+	// soyunaLechuga:="juani"
+	fmt.Println(prueba)
+	// var username string= "soy juani juanito"
+	// var prueba2="admin"
+	MetodoPapurri("sopapa", 21)
+}
+
+func MetodoPapurri(pepe string, juani int) {
+
+}
